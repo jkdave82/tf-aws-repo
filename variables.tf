b5a3ab3defc10d0292006c0420cd3d0bc2daa93a
jkdave82 <jigar.k.dave@gmail.com>
2021-12-28 13:55:51 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
+  default     =  "Jigar.k.dave-EC2-Keypair"
 }
 
 variable "instance_type" {
